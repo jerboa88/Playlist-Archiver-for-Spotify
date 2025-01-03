1a2d36fd5f0291ed31163efd659ba342e5a65009
John Goodliff <jerboa88@users.noreply.github.com>
2025-01-02 21:56:21 -0700

    Add visibility and contributions args

--- scripts/copy-playlist.py
+++ scripts/copy-playlist.py
@@ -50,6 +50,18 @@ def parse_args():
 	parser.add_argument('input_playlist_id', help=ARG_DESCS['input_playlist_id'])
 	parser.add_argument('output_playlist_name', help=ARG_DESCS['output_playlist_name'])
 	parser.add_argument(
+		'--visibility',
+		choices=['auto', 'private', 'public'],
+		default='auto',
+		help=ARG_DESCS['--contributions'],
+	)
+	parser.add_argument(
+		'--contributions',
+		choices=['auto', 'individual', 'collaborative'],
+		default='auto',
+		help=ARG_DESCS['--contributions'],
+	)
+	parser.add_argument(
 		'--debug', '-d', help=ARG_DESCS['debug'], default=False, action='store_true'
 	)
 
@@ -68,8 +80,36 @@ def parse_args():
 	return args
 
 
+# Fetch the metadata for a given playlist (fetch_fn, total_tracks, is_public, is_collaborative)
+def fetch_playlist_details(sp, input_playlist_id):
+	if input_playlist_id == SAVED_TRACKS_KEYWORD:
+		return (
+			fetch_chunk_of_saved_tracks,
+			fetch_chunk_of_saved_tracks(sp, input_playlist_id, 0, 1)['total'],
+			False,
+			False,
+		)
+	else:
+		input_playlist = sp.playlist(
+			input_playlist_id, fields='collaborative,public,tracks.total'
+		)
+
+		return (
+			fetch_chunk_of_playlist_tracks,
+			input_playlist['tracks']['total'],
+			input_playlist['public'],
+			input_playlist['collaborative'],
+		)
+
+
 # Create a new playlist with the given name
-def create_playlist(sp, user_id, playlist_name):
+def create_playlist(
+	sp,
+	user_id,
+	playlist_name,
+	is_public,
+	is_collaborative,
+):
 	logger.debug(f'Creating new playlist "{playlist_name}"')
 
 	ts = datetime.now()
@@ -78,8 +118,8 @@ def create_playlist(sp, user_id, playlist_name):
 	playlist = sp.user_playlist_create(
 		user_id,
 		playlist_name,
-		public=False,
-		collaborative=False,
+		public=is_public,
+		collaborative=is_collaborative,
 		description=f'Playlist last updated on {readable_ts} by {APP_NAME} ({APP_URL})',
 	)
 
@@ -203,21 +243,31 @@ def main():
 		SCOPE, args.client_id, args.client_secret, REDIRECT_URI, cache_handler
 	)
 	user_id = sp.me()['id']
+	(
+		fetch_fn,
+		input_playlist_num_of_tracks,
+		is_input_playlist_public,
+		is_input_playlist_collaborative,
+	) = fetch_playlist_details(sp, args.input_playlist_id)
 
-	# Use different functions to fetch tracks depending on the input playlist type
-	if args.input_playlist_id == SAVED_TRACKS_KEYWORD:
-		fetch_fn = fetch_chunk_of_saved_tracks
-	else:
-		fetch_fn = fetch_chunk_of_playlist_tracks
-
-	total_num_of_tracks = fetch_fn(sp, args.input_playlist_id, 0, 1)['total']
-
-	if total_num_of_tracks >= __PLAYLIST_TRACK_LIMIT:
-		logger.warn(
-			f'The input playlist has {total_num_of_tracks} tracks, but Spotify allows a maximum of {__PLAYLIST_TRACK_LIMIT} tracks in a playlist. We might not be able to copy them all'
+	if input_playlist_num_of_tracks >= __PLAYLIST_TRACK_LIMIT:
+		logger.warning(
+			f'The input playlist has {input_playlist_num_of_tracks} tracks, but Spotify allows a maximum of {__PLAYLIST_TRACK_LIMIT} tracks in a playlist. We might not be able to copy them all'
 		)
 
-	output_playlist_id = create_playlist(sp, user_id, args.output_playlist_name)
+	is_public = args.visibility == 'public' or (
+		args.visibility == 'auto' and is_input_playlist_public
+	)
+	is_collaborative = args.contributions == 'collaborative' or (
+		args.contributions == 'auto' and is_input_playlist_collaborative
+	)
+	output_playlist_id = create_playlist(
+		sp,
+		user_id,
+		args.output_playlist_name,
+		is_public,
+		is_collaborative,
+	)
 	track_ids_queue = queue.Queue()
 
 	threading.Thread(
@@ -227,7 +277,7 @@ def main():
 			track_ids_queue,
 			args.input_playlist_id,
 			fetch_fn,
-			total_num_of_tracks,
+			input_playlist_num_of_tracks,
 		),
 		daemon=True,
 	).start()
@@ -235,7 +285,7 @@ def main():
 		sp,
 		track_ids_queue,
 		output_playlist_id,
-		total_num_of_tracks,
+		input_playlist_num_of_tracks,
 	)
 	track_ids_queue.join()
 
